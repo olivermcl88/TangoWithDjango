ae69ecc81c0226e125a34fbacb4c9186a19f1714
olivermcl88 <2895727m@gla.ac.uk>
2024-03-31 23:21:55 +0100

    pre exercise

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
 
--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+    views.show_category, name='show_category'),
+
 ]
