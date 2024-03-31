a932ce5d9403da9501a1bcead231bdd5a2de1717
olivermcl88 <2895727m@gla.ac.uk>
2024-03-31 23:22:14 +0100

    pre exercise 2

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-
-# Update the registration to include this customised interface
-admin.site.register(Category, CategoryAdmin)
-
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -7,14 +7,13 @@ class Category(models.Model):
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
-
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
-   
+
     class Meta:
-        verbose_name_plural = 'categories'
-   
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
