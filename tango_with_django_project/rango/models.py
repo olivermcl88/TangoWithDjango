6fd0250bad48a2519b62254f4ec31ad5e64327e6
olivermcl88 <2895727m@gla.ac.uk>
2024-04-01 20:27:26 +0100

    commit 7 part 2

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    MaxLengthName = 128
+
+    name = models.CharField(max_length=MaxLengthName, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -18,8 +20,11 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    MaxLengthTitle = 128
+    
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=MaxLengthTitle)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
