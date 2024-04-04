0e7efb923535b2b96813bea18f336522d7ad3b79
olivermcl88 <2895727m@gla.ac.uk>
2024-04-04 10:04:36 +0100

    half way through 9

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,6 +6,7 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
+from rango.forms import UserForm, UserProfileForm
 
 
 def index(request):
@@ -30,30 +31,18 @@ def about(request):
 # Create your views here.
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
+
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
+
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything -
-        # the template will display the "no category" message for us.
-        context_dict['category'] = None
         context_dict['pages'] = None
-    # Go render the response and return it to the client.
+        context_dict['category'] = None
+    
     return render(request, 'rango/category.html', context=context_dict)
 
 def add_category(request):
@@ -100,4 +89,32 @@ def add_page(request, category_name_slug):
     
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+def register(request):
+
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+        
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                profile.save()
+            
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render (request, 'rango/register.html', 
+                   context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
